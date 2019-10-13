73cd952ee13094b4cbe393689fed6184cdcbefda
Humza Syed <humz_man@hotmail.com>
2019-10-13 17:31:18 +0100

    Game now takes user inputs, records the reaction times and stops the animation. No end game screen/ showing of results. Results are just printed to the console as of now
    The play button now disappears when the game starts

--- src/ReactionSpeed/ReactionSpeedGame.java
+++ src/ReactionSpeed/ReactionSpeedGame.java
@@ -4,8 +4,8 @@ import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -13,6 +13,9 @@ import java.util.concurrent.TimeUnit;
 public class ReactionSpeedGame {
 	private int attempts;
 	private float [] reactionTimes;
+
+	private boolean playerReacted;
+	private boolean colourSwapped = false;
 
 	@FXML
 	AnchorPane mainPane;
@@ -25,31 +28,52 @@ public class ReactionSpeedGame {
 	}
 
 	public void playGame(){
-		int switches = 0;
-		boolean colourSwapped = false;
-		long reactionTime = 0;
+		playGame.setVisible(false);
+
 		Random rand = new Random();
 		Scene scene = this.mainPane.getScene();
 
+		scene.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
+			if(colourSwapped)
+				playerReacted = true;
+		});
 
 		new AnimationTimer(){
 			long lastNanoTime = System.nanoTime();
 			long timeBetweenSwap = (rand.nextInt(15 - 5) + 5) * 1000;
 			int swapped =0;
 
+			long timeColourSwapped;
+			long playerReactionTime;
+
 			public void handle(long currentNanoTime){
 				long t = currentNanoTime - lastNanoTime;
 				long ms = TimeUnit.NANOSECONDS.toMillis(t);
 
-				if(ms >= timeBetweenSwap){
-					System.out.println("hi");
+				if(ms >= timeBetweenSwap && !colourSwapped){
+					timeColourSwapped = currentNanoTime - lastNanoTime;
+					colourSwapped = true;
 					timeBetweenSwap = (rand.nextInt(15 -5) + 5) * 1000;
 					changeBackground(swapped);
-					swapped++;
 					lastNanoTime = currentNanoTime;
 				}
 
-				//TODO Allow user input
+				if(playerReacted){
+					playerReactionTime = currentNanoTime - lastNanoTime;
+					playerReactionTime = TimeUnit.NANOSECONDS.toMillis(playerReactionTime);
+					reactionTimes[swapped] = playerReactionTime;
+					System.out.println(reactionTimes[swapped]);
+					swapped++;
+					playerReacted = false;
+					colourSwapped = false;
+					lastNanoTime = currentNanoTime;
+				}
+
+				if(swapped == attempts){
+					System.out.println("Game Over");
+					stop();
+				}
+
 			}
 		}.start();
 	}
